5a3dae99fe72787d71e612041bf344c743305a6f
akilan <akilankaran0512@gmail.com>
2025-09-26 22:20:06 +0530

    commit

--- shorting/bubbleSort.java
+++ shorting/bubbleSort.java
@@ -1,7 +1,31 @@
 package shorting;
 
 public class bubbleSort {
+    static void sort(int arr[],int n){
+        for(int i=0;i<n - 1;i++){
+            for(int j=0;j<n-1;j++){
+                if (arr[j]>arr[j + 1]){
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        System.out.println("after bubble short:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
-        
+        int arr[] = {13,46,24,52,20,9};
+        int n = arr.length;
+
+        System.out.println("before bubble short:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        sort(arr, n);
     }
 }
--- shorting/seletionSort.java
+++ shorting/seletionSort.java
@@ -1,11 +1,11 @@
 package shorting;
- 
+  
 public class seletionSort {
     static void sort(int arr[], int n){
         for(int i = 0;i < n-1;i++){
             int min = i;
             for(int j=i+1;j<n;j++){
-                if(arr[i]<arr[min]){
+                if(arr[j]<arr[min]){
                     min = j;
                 }
             }
@@ -31,41 +31,28 @@ public class seletionSort {
         sort(arr,n);
     }
 }
-    /* 
-public class SelectionSort {
-    public static void selectionSort(int arr[]) {
-        int n = arr.length;
+    
+/* 
+ //This is the methode to put in takeuforward and leetcode
+
+public int[] selectionSort(int[] nums) {
+
+         int n = nums.length;
 
         for (int i = 0; i < n - 1; i++) {
-            // Find the index of the minimum element
-            int minIndex = i;
+            int min = i;
             for (int j = i + 1; j < n; j++) {
-                if (arr[j] < arr[minIndex]) {
-                    minIndex = j;
+                if (nums[j] < nums[min]) {   
+                    min = j;
                 }
             }
-
-            // Swap the found minimum element with the first element
-            int temp = arr[minIndex];
-            arr[minIndex] = arr[i];
-            arr[i] = temp;
-        }
-    }
-
-    public static void main(String[] args) {
-        int arr[] = {64, 25, 12, 22, 11};
-
-        System.out.println("Original array:");
-        for (int num : arr) {
-            System.out.print(num + " ");
+            // swap
+            int temp = nums[min];
+            nums[min] = nums[i];
+            nums[i] = temp;
         }
 
-        selectionSort(arr);
-
-        System.out.println("\n\nSorted array:");
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }    
+        return nums; // return the sorted array
     }
 }
-    */
+*/
